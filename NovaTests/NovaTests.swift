366e5eab451799e59e9657d4497b85eb428d66a5
Jessica Parsons <jess@junipercreative.co>
2025-02-22 09:30:00 -0800

    Initial Commit

---
+++ NovaTests/NovaTests.swift
@@ -0,0 +1,17 @@
+//
+//  NovaTests.swift
+//  NovaTests
+//
+//  Created by Jessica Parsons on 2/22/25.
+//
+
+import Testing
+@testable import Nova
+
+struct NovaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
